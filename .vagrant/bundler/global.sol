8de002f74324e836d4d392b9fee1f4bc69b98c8d
Koju Sakaguchi <fuetf@icloud.com>
2021-04-13 13:39:20 +0900

    Revert "手順書作成"
    This reverts commit b268f32769ce6124ca0ad8ebb955605ff3acb717.

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["open4",[">= 0.4.0"]],["Platform",[">= 0.4.0"]],["popen4",["~> 0.1.2"]],["sahara",["= 0.0.17"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]]],"checksum":"8f2892177796df8886a0abdef3a0a6024583cf9f3bccab0cebaa4c303c384e4b","vagrant_version":"2.2.15"}
